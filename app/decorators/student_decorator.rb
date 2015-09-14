0c1ebc3cd929462daa0c533aa99c1b1f180c9e63
codebaker95 <codebaker95@gmail.com>
2015-09-14 23:21:03 +0200

    implement avg_notes in student decorator

--- app/decorators/student_decorator.rb
+++ app/decorators/student_decorator.rb
@@ -4,5 +4,7 @@ class StudentDecorator < BaseDecorator
   end
 
   def avg_notes(subject_item)
+    notes = subject_item_notes.where(subject_item: subject_item)
+    "%.2f" % (notes.average(:value) || 0)
   end
 end
